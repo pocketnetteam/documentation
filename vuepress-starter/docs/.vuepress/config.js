163adafdd5af3a6a4c9ea23bd3ca66fe39f845ba
gked <glyukshin@mailfence.com>
2024-09-28 08:46:56 -0700

    update the base directory

--- vuepress-starter/docs/.vuepress/config.js
+++ vuepress-starter/docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  base: '/vuepressdocs/',
+  //base: '/vuepressdocs/',
   lang: 'en-US',
   title: ' ',  
   description: 'This documentation will help you get started with Bastyon.',
